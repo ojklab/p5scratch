8410e9a9508b09abc456f8e1ff7081d0e4acd84c
OJK Lab <26958104+ojklab@users.noreply.github.com>
2022-05-21 06:33:10 +0000

    test

--- p5.scratch.js
+++ p5.scratch.js
@@ -1,33 +1,38 @@
 class Sprite {
-  constructor(_size = 50, _x = width / 2, _y = height / 2, _col = 'coral', _bg = 'white' ) {
+  constructor(
+    _size = 50,
+    _x = width / 2,
+    _y = height / 2,
+    _col = "red",
+    _bg = "white"
+  ) {
     this.x = _x;
     this.y = _y;
     this.size = _size;
-    this.dir = { x:1, y:0 };
+    this.dir = { x: 1, y: 0 };
     this.col = _col;
     this.bg = _bg;
     this.draw();
   }
-  
+
   draw() {
     noStroke();
     fill(this.col);
-    circle(this.x, this.y, this.size);    
+    circle(this.x, this.y, this.size);
   }
-  
+
   walk(steps = 10) {
     fill(this.bg);
-    circle(this.x, this.y, this.size);    
+    circle(this.x, this.y, this.size);
     this.x += this.dir.x * steps;
     this.y += this.dir.y * steps;
     this.draw();
   }
-
 }
 
-p5.prototype.startScratch = function(bg = 'white') {  
+p5.prototype.startScratch = function (bg = "white") {
   createCanvas(400, 400);
   background(bg);
   frameRate(30);
   return new Sprite();
-}
+};

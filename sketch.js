d69113ab05e7f9a3d1d5687e4fe904a23c114b36
ojklab <26958104+ojklab@users.noreply.github.com>
2022-06-23 22:43:49 +0900

    修正

--- sketch.js
+++ sketch.js
@@ -4,13 +4,6 @@
 async function setup() {
   start();
 
-  for (let y = 50; y < width; y += 50) {
-    for (let x = 50; x < width; x += 50) {
-      fill('royalblue');
-      circle(x, y, 10);
-    }
-  }
-
   for (let i = 0; i < 10; i += 1) {
     walk(10);
     await sleep(0.5);
